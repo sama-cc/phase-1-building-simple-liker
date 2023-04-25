214391f7f547250a0c2eeeb32df578b2a0983c1e
Sam <sam.camhi@gmail.com>
2023-04-25 02:58:23 -0400

    Building Simple Liker

--- main.js
+++ main.js
@@ -4,8 +4,39 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const error = document.getElementById("modal");
+error.className = "hidden";
 
+const hearts = document.querySelectorAll(".like-glyph")
 
+hearts.forEach((heart) => {
+  heart.addEventListener("click", () => {
+    if (heart.className === "like-glyph") {
+    mimicServerCall()
+    .then(() => {
+      heart.className = "activated-heart";
+      heart.innerText = `${FULL_HEART}`      
+    })
+    .catch((message) => {      
+      error.className = "";
+      error.innerText = `${message}`;
+      setTimeout(() => error.className = "hidden", 3000);
+    })
+  } else if (heart.className === "activated-heart") {
+    mimicServerCall()
+    .then(() => {
+      heart.className = "like-glyph";
+      heart.innerText = `${EMPTY_HEART}`      
+    })
+    .catch((message) => {
+      error.className = "";
+      error.innerText = `${message}`;
+      setTimeout(() => error.className = "hidden", 3000);
+    })
+  }
+}) 
+  }
+)
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
